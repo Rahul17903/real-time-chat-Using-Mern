d44631cdf198454e5c805b7629305aaa33507e20
Rahul Ghosh <rahulghosh17903@gmail.com>
2025-12-26 12:28:42 +0530

    edit axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" :`${import.meta.env.BASE_URL}/api`,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" :`https://real-time-chat-using-mern.onrender.com/api`,
   withCredentials: true,
 });
